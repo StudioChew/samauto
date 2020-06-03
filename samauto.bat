2b128efc146038cd8694bd21943f98074703de4f
StudioChew <66214382+StudioChew@users.noreply.github.com>
2020-06-03 12:01:09 +0300

    added status

--- samauto.bat
+++ samauto.bat
@@ -2,5 +2,7 @@
 title samauto     I     studio chew
 :loop
 set /p userinput="SAM: "
+title samauto     I     saying %userinput%
 sam %userinput%
+title samauto     I     said %userinput%
 goto :loop
